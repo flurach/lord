3862a53ab7ebd26569df45e8c58cdbeb112a04e6
mbugrayildiz <mbugrayildiz@outlook.com>
2020-12-26 18:55:06 +0300

    added parser framework

--- lp/parser.c
+++ lp/parser.c
@@ -2,90 +2,162 @@
 
 Node *parse(Lexer *lexer)
 {
-	Node *module = Node_new((Range){0, 0}, T_MOD, NULL);
-
-	while (1) {
-		Node *result = NULL;
-		if ((result = parse_ext(lexer)) != NULL)
-			Node_push(module, result);
-		else if ((result = parse_fdef(lexer)) != NULL)
-			Node_push(module, result);
-		else
-			break;
-	}
-
-	return module;
+	return NULL;
 }
 
-Node *parse_ext(Lexer *lexer)
+Node *parse(Lexer *lexer)
 {
-	Node *ext, *sym;
+	return NULL;
+}
 
-	if ((ext = parse_EXT(lexer)) == NULL)
-		return NULL;
+Node *parse_stmt(Lexer *lexer)
+{
+	return NULL;
+}
 
-	if ((sym = parse_SYM(lexer)) == NULL)
-		return ext;
+Node *parse_for(Lexer *lexer)
+{
+	return NULL;
+}
 
-	Node_push(ext, sym);
-	return ext;
+Node *parse_forcond(Lexer *lexer)
+{
+	return NULL;
+}
+
+Node *parse_type(Lexer *lexer)
+{
+	return NULL;
+}
+
+Node *parse_if(Lexer *lexer)
+{
+	return NULL;
+}
+
+Node *parse_else(Lexer *lexer)
+{
+	return NULL;
+}
+
+Node *parse_logic(Lexer *lexer)
+{
+	return NULL;
+}
+
+Node *parse_eqcmp(Lexer *lexer)
+{
+	return NULL;
+}
+
+Node *parse_difcmp(Lexer *lexer)
+{
+	return NULL;
 }
 
 Node *parse_fdef(Lexer *lexer)
 {
-	Node *fn, *sym, *eq, *semibind;
+	return NULL;
+}
 
-	if ((fn = parse_FN(lexer)) == NULL)
-		return NULL;
+Node *parse_fbody(Lexer *lexer)
+{
+	return NULL;
+}
 
-	if ((sym = parse_SYM(lexer)) == NULL)
-		return fn;
-	Node_push(fn, sym);
+Node *parse_bind(Lexer *lexer)
+{
+	return NULL;
+}
 
-	if ((eq = parse_EQ(lexer)) == NULL)
-		return fn;
-	Node_push(fn, eq);
+Node *parse_typedsym(Lexer *lexer)
+{
+	return NULL;
+}
 
-	if ((semibind = parse_semibind(lexer)) == NULL)
-		return fn;
-	Node_push(fn, semibind);
-
-	return fn;
+Node *parse_typeanno(Lexer *lexer)
+{
+	return NULL;
 }
 
 Node *parse_call(Lexer *lexer)
 {
-	Node *call, *sym, *arg;
-
-	if ((sym = parse_SYM(lexer)) == NULL)
-		return NULL;
-
-	if ((arg = parse_STR(lexer)) == NULL)
-		return sym;
-
-	call = Node_new(sym->range, T_CALL, NULL);
-	Node_push(call, sym);
-	Node_push(call, arg);
-
-	return call;
+	return NULL;
 }
 
-Node *parse_semibind(Lexer *lexer)
+Node *parse_expr(Lexer *lexer)
 {
-	Node *lcall, *semi, *semibind;
+	return NULL;
+}
 
-	if ((lcall = parse_call(lexer)) == NULL)
-		return NULL;
+Node *parse_term(Lexer *lexer)
+{
+	return NULL;
+}
 
-	if ((semi = parse_SEMI(lexer)) == NULL)
-		return lcall;
-	Node_push(semi, lcall);
+Node *parse_fact(Lexer *lexer)
+{
+	return NULL;
+}
 
-	if ((semibind = parse_semibind(lexer)) == NULL)
-		return semi;
-	Node_push(semi, semibind);
+Node *parse_cast(Lexer *lexer)
+{
+	return NULL;
+}
 
-	return semi;
+Node *parse_lit(Lexer *lexer)
+{
+	return NULL;
+}
+
+Node *parse_init(Lexer *lexer)
+{
+	return NULL;
+}
+
+Node *parse_initbody(Lexer *lexer)
+{
+	return NULL;
+}
+
+Node *parse_dotop(Lexer *lexer)
+{
+	return NULL;
+}
+
+Node *parse_ptr(Lexer *lexer)
+{
+	return NULL;
+}
+
+Node *parse_num(Lexer *lexer)
+{
+	return NULL;
+}
+
+Node *parse_arr(Lexer *lexer)
+{
+	return NULL;
+}
+
+Node *parse_range(Lexer *lexer)
+{
+	return NULL;
+}
+
+Node *parse_many(Lexer *lexer, Node *(*parser)(Lexer*))
+{
+	return NULL;
+}
+
+Node *parse_sepby(Lexer *lexer, Node *(*parser)(Lexer*), Node *(*delim)(Lexer*))
+{
+	return NULL;
+}
+
+Node *parse_either(Lexer *lexer, size_t count, Node *(*parser)(Lexer*))
+{
+	return NULL;
 }
 
 #define X(t)\
--- lp/parser.h
+++ lp/parser.h
@@ -3,13 +3,53 @@
 
 #include "lexer.h"
 
+/* interface */
 Node *parse(Lexer *lexer);
 
-Node *parse_ext(Lexer *lexer);
-Node *parse_fdef(Lexer *lexer);
-Node *parse_call(Lexer *lexer);
-Node *parse_semibind(Lexer *lexer);
+/* grammar */
+Node *parse_stmt(Lexer *lexer);
 
+Node *parse_for(Lexer *lexer);
+Node *parse_forcond(Lexer *lexer);
+
+Node *parse_type(Lexer *lexer);
+
+Node *parse_if(Lexer *lexer);
+Node *parse_else(Lexer *lexer);
+
+Node *parse_logic(Lexer *lexer);
+Node *parse_eqcmp(Lexer *lexer);
+Node *parse_difcmp(Lexer *lexer);
+
+Node *parse_fdef(Lexer *lexer);
+Node *parse_fbody(Lexer *lexer);
+
+Node *parse_bind(Lexer *lexer);
+Node *parse_typedsym(Lexer *lexer);
+Node *parse_typeanno(Lexer *lexer);
+
+Node *parse_call(Lexer *lexer);
+
+Node *parse_expr(Lexer *lexer);
+Node *parse_term(Lexer *lexer);
+Node *parse_fact(Lexer *lexer);
+
+Node *parse_cast(Lexer *lexer);
+Node *parse_lit(Lexer *lexer);
+Node *parse_init(Lexer *lexer);
+Node *parse_initbody(Lexer *lexer);
+Node *parse_dotop(Lexer *lexer);
+Node *parse_ptr(Lexer *lexer);
+Node *parse_num(Lexer *lexer);
+Node *parse_arr(Lexer *lexer);
+Node *parse_range(Lexer *lexer);
+
+/* higher-order parsers */
+Node *parse_many(Lexer *lexer, Node *(*parser)(Lexer*));
+Node *parse_sepby(Lexer *lexer, Node *(*parser)(Lexer*), Node *(*delim)(Lexer*));
+Node *parse_either(Lexer *lexer, size_t count, Node *(*parser)(Lexer*));
+
+/* primitive parsers */
 #define X(token) Node *parse_##token(Lexer *lexer);
 	LORD_LP_TOKENS
 #undef X
