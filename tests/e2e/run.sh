720e9ba065b456e918d116591702c5534ea9b4e1
mbugrayildiz <mbugrayildiz@outlook.com>
2021-01-08 07:18:00 +0300

    added e2e tests
    now, I do not know if this is called end to end testing.
    but let's go with that why the shoot not?

---
+++ tests/e2e/run.sh
@@ -0,0 +1,38 @@
+echo "=== E2E Testing ==="
+all="1"
+
+echo " => suite lp/lexer"
+for d in tests/e2e/lp/lexer/*; do
+	dir="${d}"
+
+	_bin/lord -l $dir/code.ld > $dir/result.out
+	cmp $dir/result.out $dir/expected.out > /dev/null
+
+	if [ $? -eq "0" ]; then
+		echo " - passed $(basename $d)"
+	else
+		echo " - failed $(basename $d)"
+		all="0"
+	fi
+done
+
+echo " => suite lp/parser"
+for d in tests/e2e/lp/parser/*; do
+	dir="${d}"
+
+	_bin/lord -p $dir/code.ld > $dir/result.out
+	cmp $dir/result.out $dir/expected.out > /dev/null
+
+	if [ $? -eq "0" ]; then
+		echo " - passed $(basename $d)"
+	else
+		echo " - failed $(basename $d)"
+		all="0"
+	fi
+done
+
+if [ $all -eq "1" ]; then
+	echo "=== ALL TESTS PASS ==="
+else
+	echo "=== SOME TESTS FAIL ==="
+fi
